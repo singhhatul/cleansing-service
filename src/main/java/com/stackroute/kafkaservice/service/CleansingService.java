0498b63015b782440bdf954df566d4ad29e2016a
atul.kumar1 <atul.kumar1@cgi.com>
2019-09-06 18:02:57 +0530

    updated kafka producer terminal to cleansing service and should produce output to kafka consumer terminal and stemming with Standford NLP

--- src/main/java/com/stackroute/kafkaservice/service/CleansingService.java
+++ src/main/java/com/stackroute/kafkaservice/service/CleansingService.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 @Service
 public class CleansingService {
 
@@ -48,6 +47,7 @@ public class CleansingService {
 
             lemma = lemma+coreLabel.lemma()+" ";
         }
+
         KafkaTemplate.send("consumer",lemma);
 
     }
